b8857e84858060011541b970730e3203fae76b80
sppp <seppo.pakonen@yandex.com>
2017-11-14 16:58:10 +0200

    Cleaning

--- src/CommandBridge/Images.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CommandBridge_Images_h_
-#define _CommandBridge_Images_h_
-
-#define IMAGECLASS CommandBridgeImg
-#define IMAGEFILE <CommandBridge/CommandBridge.iml>
-#include <Draw/iml_header.h>
-
-#endif
--- src/Earworm/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Earworm.h"
-
-#define IMAGECLASS Imgs
-#define IMAGEFILE <Earworm/Earworm.iml>
-#include <Draw/iml_source.h>
-
-GUI_APP_MAIN {
-	Earworm().Run();
-}
